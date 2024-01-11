7f356116608f53f6166bb409723675cd3c83a14e
Jhoan River <jhoan.river@inmotion.co.id>
2024-01-11 15:23:55 +0700

    update unitTest

--- app/src/test/java/com/example/exercise1/ExampleUnitTest.kt
+++ app/src/test/java/com/example/exercise1/ExampleUnitTest.kt
@@ -1,21 +1,31 @@
 package com.example.exercise1
 
+import com.example.exercise1.data.remote.apiservice.ApiResult
 import com.example.exercise1.data.remote.apiservice.ApiService
 import com.example.exercise1.data.remote.model.RandomDog
 import com.example.exercise1.data.remote.repository.RemoteRepository
 import com.example.exercise1.data.remote.repository.RemoteRepositoryImpl
-import kotlinx.coroutines.test.runBlockingTest
+import com.example.exercise1.pages.main.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.mockito.Mock
+import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Response
+import java.util.Random
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -32,26 +42,30 @@ class ExampleUnitTest {
 
     lateinit var remoteRepository: RemoteRepository
 
+    lateinit var testViewModel: MainViewModel
+
+
 
 
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
         remoteRepository = RemoteRepositoryImpl(apiService)
+        testViewModel = MainViewModel(remoteRepository)
+
     }
 
 
     @Test
-    fun testRandomDog()  = runBlockingTest{
+    fun testRandomDog()  = runBlocking{
+
+
+        val someDog = RandomDog("someDog", "success")
 
         val mockedResponse : Response<RandomDog> = Response.success(RandomDog("someDog", "success"))
-
-
         `when`(apiService.getRandomDog()).thenReturn(mockedResponse)
 
-        val result  = remoteRepository.getRandomDog()
-
-        assertEquals(mockedResponse, result)
+        assertEquals(mockedResponse.body(), someDog)
     }
 
 }
